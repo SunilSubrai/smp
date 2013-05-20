c9e034e12b8c1a2327b4b2234f3359eb9b1881db
Stacy Talbot <stacy@omobono.co.uk>
2013-05-20 13:50:24 +0100

    Fixed "More Articles Like This"

--- public_html/wp-content/themes/social-media-playbook/includes/include.article-include.php
+++ public_html/wp-content/themes/social-media-playbook/includes/include.article-include.php
@@ -9,40 +9,43 @@
 							<?php 
 								$parent 	= $post->post_parent;
 								$articles 	= get_pages('child_of=18');
-								$x 			= 0;
+								$i 			= 0;
 								$last 		= false;
 
 								foreach($articles as $article) : 
-								$x++; 
-								//since number dosen't work for some reason
-								if($x > 3) break;
+									$i++; 
 
-								if(count($articles) == $x) 
-									$last = true;
+									//since number dosen't work for some reason
+									if($i > 3) break;
+									if($post->ID != $article->ID) {
+
+										if(count($articles) == $i) 
+											$last = true;
 							?>
-							<div class="article<?php if($last) echo ' last' ?>">
-								<div class="thumb">
-									<div class="new">
-										<img src="<?php echo image_url(); ?>/new-label_1024.png">
-									</div>
-									<?php
-										$x 		= get_field( "post_heading", $article->ID );
-										$url 	= $x["url"];
-										$alt  	= $x["alt"];
-									?>
-									<img src="<?php echo $url; ?>" alt="<?php echo $alt ?>" />
-								</div>
-								<div class="description">
-									<h3><a href="#"><?php echo $article->post_title ?></a></h3>
-									<p><?php echo $article->post_excerpt ?></p>
-								</div>
-								<a href="<?php echo get_permalink($article->ID) ?>" class="read-button">
-									<img border="0" src="<?php echo image_url(); ?>/read-button_1024.png">
-								</a>
-								
-								<div class="line"></div>
-							</div>
-							<?php endforeach; ?>
+										<div class="article<?php if($last) echo ' last' ?>">
+											<div class="thumb">
+												<div class="new">
+													<img src="<?php echo image_url(); ?>/new-label_1024.png">
+												</div>
+												<?php
+													$x 		= get_field( "post_heading", $article->ID );
+													$url 	= $x["url"];
+													$alt  	= $x["alt"];
+												?>
+												<img src="<?php echo $url; ?>" alt="<?php echo $alt ?>" />
+											</div>
+											<div class="description">
+												<h3><a href="<?php echo get_permalink($article->ID) ?>"><?php echo $article->post_title ?></a></h3>
+												<p><?php echo $article->post_excerpt ?></p>
+											</div>
+											<a href="<?php echo get_permalink($article->ID) ?>" class="read-button">
+												<img border="0" src="<?php echo image_url(); ?>/read-button_1024.png">
+											</a>
+											
+											<div class="line"></div>
+										</div>
+									<?php } ?>
+								<?php endforeach; ?>
 
 						</div>
 					</div>
